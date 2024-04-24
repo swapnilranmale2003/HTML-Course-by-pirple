cdd8e290ca71b20971c45e14edc99a66732c7d2c
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-24 13:00:03 +0530

    task4 is done

--- javascriptquestion/script.js
+++ javascriptquestion/script.js
@@ -1,1 +1,23 @@
 console.log("Good Morning!")
+
+
+// {
+//   var a = 5;   //var is global scope & let, const has local scope
+// }
+// console.log(a)
+
+//illegal shadowing
+
+function test (){
+    var a = "Hello";
+    var b = "bye"
+    if(true){
+       let a = "Hii"
+       let b = "goodbye"
+        console.log(a)
+        console.log(b)
+    }
+    console.log(a)
+}
+
+test()
